6f338ea99dfe4a14fbcc3e0dc0a94e00608dddac
Ye4y <Arman.Alemirad@gmail.com>
2023-01-30 17:40:48 +0100

    Prepare IT test for Redis backed cache

---
+++ src/main/java/App.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        char operator;
+        double number1, number2;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Choose an operator: +, -, *, or /");
+        operator = input.next().charAt(0);
+
+        System.out.println("Enter first number");
+        number1 = input.nextDouble();
+
+        System.out.println("Enter second number");
+        number2 = input.nextDouble();
+
+        switch (operator) {
+            case '+' -> System.out.printf("%s + %s = %s%n", number1, number2, number1 + number2);
+            case '-' -> System.out.printf("%s - %s = %s%n", number1, number2, number1 - number2);
+            case '*' -> System.out.printf("%s * %s = %s%n", number1, number2, number1 * number2);
+            case '/' -> System.out.printf("%s / %s = %s%n", number1, number2, number1 / number2);
+            default -> System.out.println("Invalid operator!");
+        }
+        input.close();
+    }
+}
